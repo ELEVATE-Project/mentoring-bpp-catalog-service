a0ecfa7409d475513e34a7c0a2e9a558ce956951
joffinjoy <joffinjoy@gmail.com>
2023-09-01 18:04:18 +0530

    Modified Elastic Schema

--- src/constants/objectTemplates/agent.js
+++ src/constants/objectTemplates/agent.js
@@ -1,5 +1,30 @@
 'use strict'
 
+const { faker } = require('@faker-js/faker')
+
+const states = ['Odisha', 'Gujarat', 'Kerala', 'Sikkim', 'Rajasthan', 'Karnataka', 'Maharashtra', 'Punjab']
+
+const stateCreator = () => {
+	return faker.helpers.arrayElement(states)
+}
+
+const capacities = [50, 100, 200, 300]
+
+const capacityCreator = () => {
+	return faker.helpers.arrayElement(capacities)
+}
+
+const facilities = ['ac', 'wifi', 'smartclass', 'catering', 'parking']
+
+const facilitiesCreator = () => {
+	return faker.helpers.arrayElements(facilities)
+}
+exports.agentHandlers = {
+	stateCreator,
+	capacityCreator,
+	facilitiesCreator,
+}
+
 exports.agentTemplate = {
 	person: {
 		id: '{{mentor._id}}',
@@ -7,7 +32,12 @@ exports.agentTemplate = {
 		image: {
 			url: '{{mentor.image}}',
 		},
-		gender: '{{mentor.gender}}',
+		state: '=> stateCreator()',
+		capacity: '=> capacityCreator()',
+		facilities: '=> facilitiesCreator()',
+		orgName: '{{organization.name}}',
+		date: '{{startDateUtc}}',
+		sessionId: '{{_id}}',
+		fulfillment_ids: ['{{fulfillmentId}}'],
 	},
-	rating: '{{mentor.rating.average}}',
 }
--- src/helpers/objectTransformers.js
+++ src/helpers/objectTransformers.js
@@ -4,11 +4,11 @@ const { objectTransformer } = require('@utils/objectTransformer')
 const { sessionTemplate, sessionHandlers } = require('@constants/objectTemplates/session')
 const { fulfillmentTemplate, fulfillmentHandlers } = require('@constants/objectTemplates/fulfillment')
 const { providerTemplate } = require('@constants/objectTemplates/provider')
-const { agentTemplate } = require('@constants/objectTemplates/agent')
+const { agentTemplate, agentHandlers } = require('@constants/objectTemplates/agent')
 
 exports.objectTransformers = {
 	session: objectTransformer(sessionTemplate, sessionHandlers),
-	agent: objectTransformer(agentTemplate),
+	agent: objectTransformer(agentTemplate, agentHandlers),
 	provider: objectTransformer(providerTemplate),
 	fulfillment: objectTransformer(fulfillmentTemplate, fulfillmentHandlers),
 }
