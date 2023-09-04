bdb8c30bb9c61a4988ef84740672ec1b7161bcba
joffinjoy <joffinjoy@gmail.com>
2023-09-04 22:04:14 +0530

    Branching Fix

--- src/helpers/fulfillmentAndAgentObjects.js
+++ src/helpers/fulfillmentAndAgentObjects.js
@@ -4,8 +4,12 @@ const agentQueries = require('@database/indexing/agent/queries')
 
 exports.getFulfillmentAndAgentObjects = async (fulfillmentId, time) => {
 	const fulfillmentDoc = await fulfillmentQueries.findById(fulfillmentId, time)
-	const fulfillment = fulfillmentDoc ? fulfillmentDoc._source : null //Use Generic Function Here
-	const agentDoc = await agentQueries.findById(fulfillment.agentId)
-	const agent = agentDoc._source //Use Generic Function Here
-	return { fulfillment, agent }
+	if (fulfillmentDoc) {
+		const fulfillment = fulfillmentDoc._source //Use Generic Function Here
+		const agentDoc = await agentQueries.findById(fulfillment.agentId)
+		const agent = agentDoc._source //Use Generic Function Here
+		return { fulfillment, agent }
+	} else {
+		return { fulfillment: null, agent: null }
+	}
 }
